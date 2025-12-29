c1cff5dd31f9f02c2c4a699b8fa1658323bb352c
remnantcjy <remnantcjy@gmail.com>
2025-12-29 18:04:20 +0900

    ✨ feat: Level 2-8. QueryDSL
    - build.gradle querydsl 종속성 추가
    - QuerydslConfig 생성
    - TodoRepository 기존 findByIdWithUser() 주석
    - TodoCustomRepository findByIdWithUser() 추가
    - TodoCustomRepositoryImpl findByIdWithUser()에 querydsl 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -11,15 +11,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
+    /*
+    // QueryDSL 사용 !
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
+            "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+     */
 
     @Query("SELECT t FROM  Todo  t " +
             "where (:weather IS NULL or t.weather = :weather) " +
